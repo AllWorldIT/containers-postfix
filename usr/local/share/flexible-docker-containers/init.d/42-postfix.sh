6fa559d286f7e454d3ed8cfbeaf6cb08397a226d
Nigel Kukard <nkukard@lbsd.net>
2023-01-21 22:23:39 +0000

    feat: migrated to FDC logging

--- usr/local/share/flexible-docker-containers/init.d/42-postfix.sh
+++ usr/local/share/flexible-docker-containers/init.d/42-postfix.sh
@@ -21,13 +21,12 @@
 
 
 # Check if we need to enable postfix
-if [ -z "$POSTFIX_ROOT_ADDRESS" -o -z "$POSTFIX_MYHOSTNAME" -o -z "$POSTFIX_RELAYHOST" ]; then
-	echo "NOTICE: Disabling Postfix (POSTFIX_ROOT_ADDRESS, POSTFIX_MYHOSTNAME, POSTFIX_RELAYHOST not set)"
+if [ -z "$POSTFIX_ROOT_ADDRESS" ] || [ -z "$POSTFIX_MYHOSTNAME" ] || [ -z "$POSTFIX_RELAYHOST" ]; then
+	fdc_notice "Disabling Postfix (POSTFIX_ROOT_ADDRESS, POSTFIX_MYHOSTNAME, POSTFIX_RELAYHOST not set)"
 	return
-else
-    echo "NOTICE: Enabling Postfix"
 fi
 
+fdc_notice "Initializing Postfix settings"
 
 # Check configurable options
 if [ -z "$POSTFIX_ABUSE_ADDRESS" ]; then
@@ -63,68 +62,85 @@ mv /etc/supervisor/conf.d/postfix.conf.disabled /etc/supervisor/conf.d/postfix.c
 # Remove old inet_protocols option
 sed -e '/^inet_protocols =/d' -i /etc/postfix/main.cf
 
-echo "### START DOCKER CONFIG ###" >> /etc/postfix/main.cf
-echo "inet_protocols = all" >> /etc/postfix/main.cf
-echo "myhostname = $POSTFIX_MYHOSTNAME" >> /etc/postfix/main.cf
-echo "smtpd_banner = \$myhostname ESMTP" >> /etc/postfix/main.cf
-echo "mydestination = \$myhostname, $HOSTNAME$POSTFIX_DESTINATIONS, localhost.localdomain, localhost" >> /etc/postfix/main.cf
-echo "relayhost = $POSTFIX_RELAYHOST" >> /etc/postfix/main.cf
-echo "# Output logs to stdout" >> /etc/postfix/main.cf
-echo "maillog_file = /dev/stdout" >> /etc/postfix/main.cf
-echo "disable_vrfy_command = yes" >> /etc/postfix/main.cf
-echo "smtpd_helo_required = yes" >> /etc/postfix/main.cf
-echo "strict_rfc821_envelopes = yes" >> /etc/postfix/main.cf
+{
+	echo "### START DOCKER CONFIG ###"
+	echo "inet_protocols = all"
+	echo "myhostname = $POSTFIX_MYHOSTNAME"
+	echo "smtpd_banner = \$myhostname ESMTP"
+	echo "mydestination = \$myhostname, $HOSTNAME$POSTFIX_DESTINATIONS, localhost.localdomain, localhost"
+	echo "relayhost = $POSTFIX_RELAYHOST"
+	echo "# Output logs to stdout"
+	echo "maillog_file = /dev/stdout"
+	echo "disable_vrfy_command = yes"
+	echo "smtpd_helo_required = yes"
+	echo "strict_rfc821_envelopes = yes"
 
-echo "message_size_limit = 102400000" >> /etc/postfix/main.cf
-echo "mailbox_size_limit = 0" >> /etc/postfix/main.cf
+	echo "message_size_limit = 102400000"
+	echo "mailbox_size_limit = 0"
 
-echo "enable_long_queue_ids = yes" >> /etc/postfix/main.cf
+	echo "enable_long_queue_ids = yes"
 
-echo "address_verify_map = lmdb:/var/lib/postfix/verify" >> /etc/postfix/main.cf
-echo "address_verify_negative_refresh_time = 5m" >> /etc/postfix/main.cf
-echo "unverified_recipient_reject_code = 550" >> /etc/postfix/main.cf
-echo "unverified_sender_reject_code = 550" >> /etc/postfix/main.cf
-echo "unknown_address_reject_code = 550" >> /etc/postfix/main.cf
-echo "unverified_recipient_reject_reason = Recipient address verification failed" >> /etc/postfix/main.cf
+	echo "address_verify_map = lmdb:/var/lib/postfix/verify"
+	echo "address_verify_negative_refresh_time = 5m"
+	echo "unverified_recipient_reject_code = 550"
+	echo "unverified_sender_reject_code = 550"
+	echo "unknown_address_reject_code = 550"
+	echo "unverified_recipient_reject_reason = Recipient address verification failed"
 
-echo "smtpd_relay_restrictions = permit_mynetworks, reject_unauth_destination" >> /etc/postfix/main.cf
-echo "smtpd_recipient_restrictions = reject_non_fqdn_recipient, reject_unknown_recipient_domain, permit_mynetworks, warn_if_reject reject_unknown_reverse_client_hostname, reject_invalid_helo_hostname, reject_non_fqdn_helo_hostname, warn_if_reject reject_unknown_helo_hostname, reject_unauth_destination, reject_unverified_recipient" >> /etc/postfix/main.cf
-echo "smtpd_sender_restrictions = reject_non_fqdn_sender, reject_unknown_sender_domain" >> /etc/postfix/main.cf
-echo "smtpd_data_restrictions = reject_multi_recipient_bounce, reject_unauth_pipelining" >> /etc/postfix/main.cf
+	echo "smtpd_relay_restrictions = permit_mynetworks, reject_unauth_destination"
+	echo "smtpd_recipient_restrictions = reject_non_fqdn_recipient, reject_unknown_recipient_domain, permit_mynetworks, warn_if_reject reject_unknown_reverse_client_hostname, reject_invalid_helo_hostname, reject_non_fqdn_helo_hostname, warn_if_reject reject_unknown_helo_hostname, reject_unauth_destination, reject_unverified_recipient"
+	echo "smtpd_sender_restrictions = reject_non_fqdn_sender, reject_unknown_sender_domain"
+	echo "smtpd_data_restrictions = reject_multi_recipient_bounce, reject_unauth_pipelining"
 
-echo "relay_domains = /etc/postfix/relay_domains" >> /etc/postfix/main.cf
-echo "transport_maps = lmdb:/etc/postfix/transport_maps" >> /etc/postfix/main.cf
-echo "### END DOCKER CONFIG ###" >> /etc/postfix/main.cf
+	echo "relay_domains = /etc/postfix/relay_domains"
+	echo "transport_maps = lmdb:/etc/postfix/transport_maps"
+	echo "### END DOCKER CONFIG ###"
+
+} >> /etc/postfix/main.cf
 
 # Master
 touch /etc/postfix/master.cf
-echo "$POSTFIX_MASTER_CF" | while read line; do
+echo "$POSTFIX_MASTER_CF" | while read -r line; do
 	echo "$line" >> /etc/postfix/master.cf
 done
 
 # Relay domains
 touch /etc/postfix/relay_domains
-echo "$POSTFIX_RELAY_DOMAINS" | while read domain; do
+echo "$POSTFIX_RELAY_DOMAINS" | while read -r domain; do
 	echo "$domain" >> /etc/postfix/relay_domains
 done
 
 # Transport maps
 touch /etc/postfix/transport_maps
-echo "$POSTFIX_TRANSPORT_MAPS" | while read line; do
+echo "$POSTFIX_TRANSPORT_MAPS" | while read -r line; do
 	echo "$line" >> /etc/postfix/transport_maps
 done
 postmap /etc/postfix/transport_maps
 
 # Setup aliases
-echo "### START DOCKER CONFIG ###" > /etc/postfix/aliases
-echo "root: $POSTFIX_ROOT_ADDRESS" >> /etc/postfix/aliases
-echo "abuse: $POSTFIX_ABUSE_ADDRESS" >> /etc/postfix/aliases
-echo "admin: $POSTFIX_ADMIN_ADDRESS" >> /etc/postfix/aliases
-echo "administrator: $POSTFIX_ADMINISTRATOR_ADDRESS" >> /etc/postfix/aliases
-echo "webmaster: $POSTFIX_WEBMASTER_ADDRESS" >> /etc/postfix/aliases
-echo "postmaster: $POSTFIX_POSTMASTER_ADDRESS" >> /etc/postfix/aliases
-echo "hostmaster: $POSTFIX_HOSTMASTER_ADDRESS" >> /etc/postfix/aliases
-echo "noreply: $POSTFIX_NOREPLY_ADDRESS" >> /etc/postfix/aliases
-echo "### END DOCKER CONFIG ###" >> /etc/postfix/aliases
+{
+	echo "### START DOCKER CONFIG ###"
+	echo "root: $POSTFIX_ROOT_ADDRESS"
+	echo "abuse: $POSTFIX_ABUSE_ADDRESS"
+	echo "admin: $POSTFIX_ADMIN_ADDRESS"
+	echo "administrator: $POSTFIX_ADMINISTRATOR_ADDRESS"
+	echo "webmaster: $POSTFIX_WEBMASTER_ADDRESS"
+	echo "postmaster: $POSTFIX_POSTMASTER_ADDRESS"
+	echo "hostmaster: $POSTFIX_HOSTMASTER_ADDRESS"
+	echo "noreply: $POSTFIX_NOREPLY_ADDRESS"
+	echo "### END DOCKER CONFIG ###"
+} > /etc/postfix/aliases
 
 newaliases
+
+# Make sure the spool directories exist
+for i in bounce corrupt defer deferred flush hold incoming maildrop pid private public saved trace; do
+	if [ ! -d "/var/spool/postfix/$i" ]; then
+		mkdir "/var/spool/postfix/$i"
+	fi
+done
+
+# Fix spool directory permissions
+chown postfix /var/spool/postfix/* /var/lib/postfix
+chown root:postfix /var/spool/postfix/pid
+chgrp postdrop /var/spool/postfix/maildrop /var/spool/postfix/public
